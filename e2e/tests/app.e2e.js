364a9ffaa674d758dd48ccad17060ba5d81174d1
NicoColman <nico@ArchNico.localdomain>
2025-11-24 11:33:55 +0000

    Intentado arreglar e2e

--- e2e/tests/app.e2e.js
+++ e2e/tests/app.e2e.js
@@ -4,14 +4,14 @@ Scenario('Add and remove item', async ({ I }) => {
   const itemName = 'Test Item ' + Date.now();
 
   I.amOnPage('/');
-  I.waitForText('Items', 5);
+  I.waitForText('Items', 30);
 
   // Add item
   I.fillField('Nuevo item', itemName);
   I.click('Agregar');
 
   // Verify item exists
-  I.waitForText(itemName, 5);
+  I.waitForText(itemName, 30);
 
   // Delete item
   // We find the delete button inside the list item that contains our item name
