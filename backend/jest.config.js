1a10837cc41a2182ed4768b68f04209eb8668fc3
NicoColman <nico@ArchNico.localdomain>
2025-11-26 11:29:40 +0000

    reports de vuelta

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
+  reporters: [
+    ['jest-html-reporter', {
+      pageTitle: 'Backend Test Report',
+      outputPath: 'test-report.html',
+    }],
+    ['jest-junit', {
+      outputDirectory: '.',
+      outputName: 'junit.xml',
+    }]
+  ],
 };
-
-
